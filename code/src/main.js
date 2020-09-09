03c79b4764ed253032e89622a48a0f2220497630
Amy Y <ctrlalternativedelete@gmail.com>
2020-09-09 16:01:11 +1000

    fixed spa for ghpages

--- code/src/main.js
+++ code/src/main.js
@@ -9,5 +9,12 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    if (sessionStorage.redirect) {
+      const redirect = sessionStorage.redirect
+      delete sessionStorage.redirect
+      this.$router.push(redirect)
+    }
+  }
 }).$mount('#app')
