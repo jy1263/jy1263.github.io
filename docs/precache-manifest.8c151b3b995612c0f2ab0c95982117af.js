7eaf3a53dc0d35901aa27438c2d15372245384d7
Amy Y <ctrlalternativedelete@gmail.com>
2020-10-06 22:14:13 +1100

    update dist

---
+++ docs/precache-manifest.8c151b3b995612c0f2ab0c95982117af.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c7bf8f1879a77229eaab505c6f4e74",
+    "url": "/404.html"
+  },
+  {
+    "revision": "1b8fa91cc3a8f3f02bc3",
+    "url": "/css/about.9e246519.css"
+  },
+  {
+    "revision": "08466bc5bf107c574641",
+    "url": "/css/app.41f66b10.css"
+  },
+  {
+    "revision": "421707b784a9ce3243b4",
+    "url": "/css/chunk-vendors.43146d95.css"
+  },
+  {
+    "revision": "31d47085569e772c0f57aa8ec8381a5c",
+    "url": "/fonts/materialdesignicons-webfont.31d47085.woff"
+  },
+  {
+    "revision": "4a837d054b5f2a37170df8a275a13816",
+    "url": "/fonts/materialdesignicons-webfont.4a837d05.eot"
+  },
+  {
+    "revision": "b0fd91bb29dcb296a9a37f8bda0a2d85",
+    "url": "/fonts/materialdesignicons-webfont.b0fd91bb.ttf"
+  },
+  {
+    "revision": "f1997a8aba8a498fe4032e3b56e871ca",
+    "url": "/fonts/materialdesignicons-webfont.f1997a8a.woff2"
+  },
+  {
+    "revision": "f21308257693373b1adc82f0cf15f092",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b8fa91cc3a8f3f02bc3",
+    "url": "/js/about.4ead2909.js"
+  },
+  {
+    "revision": "08466bc5bf107c574641",
+    "url": "/js/app.b99ce624.js"
+  },
+  {
+    "revision": "421707b784a9ce3243b4",
+    "url": "/js/chunk-vendors.a59aadd9.js"
+  },
+  {
+    "revision": "5f513f2d6aa61bf62b3719ea21a3c746",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
